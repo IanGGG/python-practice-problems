8eae85ea98408c80fbf90fae8585ba76ba23144c
Xuefei Gao <xuefei@uchicago.edu>
2023-04-23 17:38:45 -0500

    finished all, except printing

--- problems/trees/bst.py
+++ problems/trees/bst.py
@@ -22,6 +22,9 @@ class Empty:
     def insert(self, n):
         return Node(n, Empty(), Empty())
 
+    def inorder(self):
+        return []
+
 
 class Node:
 
@@ -58,9 +61,65 @@ class Node:
         else:
             return self
 
+    def inorder(self):
+        if self.is_leaf():
+            return [self.value]
+        else:
+            return self.left.inorder() + [self.value] + self.right.inorder()
+
+    def min_item(self):
+        if self.left.is_empty():
+            return self.value
+        else:
+            return self.left.min_item()
+
+    def max_item(self):
+        if self.right.is_empty():
+            return self.value
+        else:
+            return self.right.max_item()
+
+    def balance_factor(self):
+        return self.right.height() - self.left.height()
+
+    def balanced_everywhere(self):
+        if self.is_leaf():
+            return True
+        else:
+            if self.balance_factor() not in [-1, 0, 1]:
+                return False
+            else:
+                return (self.left.balanced_everywhere()
+                        and self.right.balanced_everywhere())
+
+    def add_to_all(self, n):
+        self.value += n
+        if not self.left.is_empty():
+            self.left.add_to_all(n)
+        if not self.right.is_empty():
+            self.right.add_to_all(n)
+
+    def path_to(self, n):
+        if not self.contains(n):
+            return None
+
+        if self.value == n:
+            return [n]
+        if self.value > n:
+            return [self.value] + self.left.path_to(n)
+        if self.value < n:
+            return [self.value] + self.right.path_to(n)
+
 
 if __name__ == "__main__":
-    bst = Empty().insert(42).insert(10).insert(15).insert(63)
-
+    bst = Empty().insert(42).insert(10).insert(15).insert(63).insert(23)
     print(f"The number of nodes is {bst.num_nodes()}")
     print(f"The height is {bst.height()}")
+    print(bst.inorder())
+    print(f"The min is {bst.min_item()}")
+    print(f"The max is {bst.max_item()}")
+    print(f"The balance factor is {bst.balance_factor()}")
+    print(f"Balance every where? {bst.balanced_everywhere()}")
+    print(f"Path to {23} {bst.path_to(23)}")
+    bst.add_to_all(3)
+    print(bst.inorder())
